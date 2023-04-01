1a8bd14d255989e252452047ec1bf6cd3d334e8f
unknown <jonasleonhardfl@gmail.com>
2023-04-01 16:51:01 +0200

    fix(indent-blankline): added current context and color (#theme)

--- lua/user/plugins/indent-blankline.lua
+++ lua/user/plugins/indent-blankline.lua
@@ -1,12 +1,17 @@
 return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    event = "VeryLazy",
+    event = 'VeryLazy',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
       char = '┊',
       show_trailing_blankline_indent = false,
+      show_current_context = true,
     },
+    config = function(_, opts)
+      vim.cmd [[highlight IndentBlanklineContextChar guifg=#585B70 gui=nocombine]]
+      require('indent_blankline').setup(opts)
+    end,
   },
 }
