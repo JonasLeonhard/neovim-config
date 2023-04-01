007cdfdbd8474b74f9b1413ed9f04252ce8813e2
unknown <jonasleonhardfl@gmail.com>
2023-04-01 15:42:45 +0200

    feat(theme): added theme integrations (#

--- lua/user/plugins/lsp/navic.lua
+++ lua/user/plugins/lsp/navic.lua
@@ -1,7 +1,37 @@
 return {
   "SmiteshP/nvim-navic",
   dependencies = { "neovim/nvim-lspconfig" },
-  config = function ()
-    require("nvim-navic").setup()
+  config = function()
+    require("nvim-navic").setup({
+      highlight = true,
+      icons = {
+        File = ' ',
+        Module = ' ',
+        Namespace = ' ',
+        Package = ' ',
+        Class = ' ',
+        Method = ' ',
+        Property = ' ',
+        Field = ' ',
+        Constructor = ' ',
+        Enum = ' ',
+        Interface = ' ',
+        Function = ' ',
+        Variable = ' ',
+        Constant = ' ',
+        String = ' ',
+        Number = ' ',
+        Boolean = ' ',
+        Array = ' ',
+        Object = ' ',
+        Key = ' ',
+        Null = ' ',
+        EnumMember = ' ',
+        Struct = ' ',
+        Event = ' ',
+        Operator = ' ',
+        TypeParameter = ' '
+      },
+    })
   end
 }
--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -3,6 +3,21 @@ return {
     "catppuccin/nvim", name = "catppuccin",
     opts = {
       flavour = 'mocha',
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        telescope = true,
+        navic = true,
+        dap = true,
+        bufferline = true,
+        hop = true,
+        mason = true,
+        noice = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+        harpoon = true
+      }
     },
     config = function(_, opts)
       require('catppuccin').setup(opts)
