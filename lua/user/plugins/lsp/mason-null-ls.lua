d76ed5d5c074c5ef06862a3a56dd602faed88549
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-06-15 18:29:35 +0200

    feat(null-ls): yarn berry not finding config file with eslint_d (#linting)
    There is an issue, where eslint_d cannot resolve dependencies of yarn
    berry when pnp dependencies are enabled.
    
    This commit:
    
      - switches to plain eslint, if yarn berry .pnp.cjs file is found. In
        order to achive this, mason-null-ls automatic setup was disabled for
        eslint_d

--- lua/user/plugins/lsp/mason-null-ls.lua
+++ lua/user/plugins/lsp/mason-null-ls.lua
@@ -12,6 +12,10 @@ return {
       local mason_null_ls = require 'mason-null-ls'
       local null_ls = require 'null-ls'
 
+      -- Extend null_ls builtins before they are beeing setup:
+      vim.list_extend(null_ls.builtins.formatting.prettier.filetypes, { 'twig', 'svelte' })
+      vim.list_extend(null_ls.builtins.diagnostics.eslint.filetypes, { 'svelte' })
+
       mason_null_ls.setup {
         ensure_installed = {
           -- Opt to list sources here, when available in mason.
@@ -22,18 +26,45 @@ return {
         },
         automatic_installation = false,
         automatic_setup = true, -- Recommended, but optional
-        handlers = {},
+        handlers = {
+          eslint_d = function() end, -- disable automatic setup: see (1)
+        },
       }
 
-      -- Extend null_ls builtins before they are beeing setup:
-      vim.list_extend(null_ls.builtins.formatting.prettier.filetypes, { 'twig', 'svelte' })
-      vim.list_extend(null_ls.builtins.diagnostics.eslint.filetypes, { 'svelte' })
+      -- So we check if a .pnp.cjs file is in the project and switch to plain eslint
+
+      -- handling yarn berry and pnpm:
+      local command_resolver = require 'null-ls.helpers.command_resolver'
 
       null_ls.setup {
+        -- Add Anything not supported by mason or extended edgecases.
         sources = {
-          -- Add Anything not supported by mason or extended.
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.diagnostics.eslint,
+          -- (1) We can use `eslint_d` if we don't use Yarn PnP.
+          null_ls.builtins.diagnostics.eslint_d.with {
+            condition = function(utils)
+              return not utils.root_has_file '.pnp.cjs'
+            end,
+          },
+          null_ls.builtins.code_actions.eslint_d.with {
+            condition = function(utils)
+              return not utils.root_has_file '.pnp.cjs'
+            end,
+          },
+
+          -- (1) We have to fall back to `eslint` if we use Yarn PnP. `eslint_d` is not able to resolve
+          -- plugins inside the `eslintrc.cjs` configuration file.
+          null_ls.builtins.diagnostics.eslint.with {
+            dynamic_command = command_resolver.from_yarn_pnp(),
+            condition = function(utils)
+              return utils.root_has_file '.pnp.cjs'
+            end,
+          },
+          null_ls.builtins.code_actions.eslint.with {
+            dynamic_command = command_resolver.from_yarn_pnp(),
+            condition = function(utils)
+              return utils.root_has_file '.pnp.cjs'
+            end,
+          },
         },
       }
     end,
