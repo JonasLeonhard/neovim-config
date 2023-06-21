69789cc77ddfd4efd893d90cbf45af1ff8bb7b08
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-06-21 15:26:26 +0200

    fix(illuminate): fixed vim-illuminate causing lag for large filesizes

--- lua/user/plugins/vim-illuminate.lua
+++ lua/user/plugins/vim-illuminate.lua
@@ -3,7 +3,13 @@ return {
     'RRethy/vim-illuminate',
     event = { 'BufReadPost', 'BufNewFile' },
     lazy = true,
-    opts = { delay = 200 },
+    opts = {
+      delay = 200,
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { 'lsp' },
+      },
+    },
     config = function(_, opts)
       require('illuminate').configure(opts)
     end,
