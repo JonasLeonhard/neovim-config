f44f8f13dd558c9ca3df43d0b71976abe2ae04b6
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-07-27 16:46:51 +0200

    fix(flash): move to fuzzy finding

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -1,43 +1,3 @@
----@param opts Flash.Format
-local function format(opts)
-  -- always show first and second label
-  return {
-    { opts.match.label1, 'FlashMatch' },
-    { opts.match.label2, 'FlashLabel' },
-  }
-end
-
-local twoCharLabel = { after = false, before = { 0, 0 }, uppercase = false, format = format }
-
-local twoCharActionRemote = function(match, state)
-  state:hide()
-  require('flash').remote {
-    search = { max_length = 0 },
-    highlight = { matches = false },
-    label = { format = format },
-    matcher = function(win)
-      -- limit matches to the current label
-      return vim.tbl_filter(function(m)
-        return m.label == match.label and m.win == win
-      end, state.results)
-    end,
-    labeler = function(matches)
-      for _, m in ipairs(matches) do
-        m.label = m.label2 -- use the second label
-      end
-    end,
-  }
-end
-
-local twoCharLabeler = function(matches, state)
-  local labels = state:labels()
-  for m, match in ipairs(matches) do
-    match.label1 = labels[math.floor((m - 1) / #labels) + 1]
-    match.label2 = labels[(m - 1) % #labels + 1]
-    match.label = match.label1
-  end
-end
-
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
@@ -76,11 +36,7 @@ return {
       mode = 'o',
       function()
         require('flash').remote {
-          search = { mode = 'search' },
-          label = twoCharLabel,
-          pattern = [[\<]],
-          action = twoCharActionRemote,
-          labeler = twoCharLabeler,
+          search = { mode = 'fuzzy' },
         }
       end,
       desc = 'Remote Flash',
