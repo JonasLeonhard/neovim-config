a905af0227b1fe15aae6ba2899d2021f355b8d42
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-07-27 15:58:43 +0200

    fix(flash): added multi select for search mode

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -1,57 +1,97 @@
+---@param opts Flash.Format
+local function format(opts)
+  -- always show first and second label
+  return {
+    { opts.match.label1, 'FlashMatch' },
+    { opts.match.label2, 'FlashLabel' },
+  }
+end
+
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
+  'folke/flash.nvim',
+  event = 'VeryLazy',
   ---@type Flash.Config
   opts = {
     modes = {
       search = {
-        enabled = false
+        enabled = false,
       },
       char = {
-        label = { exclude = "hjkliardcwbyog" },
-      }
-    }
+        label = { exclude = 'hjkliardcwbyog' },
+      },
+    },
   },
   keys = {
     {
-      "s",
-      mode = { "n", "x", "o" },
+      's',
+      mode = { 'n', 'x', 'o' },
       function()
-        require("flash").jump()
+        require('flash').jump {
+          search = { mode = 'search' },
+          label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+          pattern = [[\<]],
+          action = function(match, state)
+            state:hide()
+            require('flash').jump {
+              search = { max_length = 0 },
+              highlight = { matches = false },
+              label = { format = format },
+              matcher = function(win)
+                -- limit matches to the current label
+                return vim.tbl_filter(function(m)
+                  return m.label == match.label and m.win == win
+                end, state.results)
+              end,
+              labeler = function(matches)
+                for _, m in ipairs(matches) do
+                  m.label = m.label2 -- use the second label
+                end
+              end,
+            }
+          end,
+          labeler = function(matches, state)
+            local labels = state:labels()
+            for m, match in ipairs(matches) do
+              match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+              match.label2 = labels[(m - 1) % #labels + 1]
+              match.label = match.label1
+            end
+          end,
+        }
       end,
-      desc = "Flash",
+      desc = 'Flash',
     },
     {
-      "S",
-      mode = { "n", "o", "x" },
+      'S',
+      mode = { 'n', 'o', 'x' },
       function()
-        require("flash").treesitter()
+        require('flash').treesitter()
       end,
-      desc = "Flash Treesitter",
+      desc = 'Flash Treesitter',
     },
     {
-      "r",
-      mode = "o",
+      'r',
+      mode = 'o',
       function()
-        require("flash").remote()
+        require('flash').remote()
       end,
-      desc = "Remote Flash",
+      desc = 'Remote Flash',
     },
     {
-      "R",
-      mode = { "o", "x" },
+      'R',
+      mode = { 'o', 'x' },
       function()
-        require("flash").treesitter_search()
+        require('flash').treesitter_search()
       end,
-      desc = "Flash Treesitter Search",
+      desc = 'Flash Treesitter Search',
     },
     {
-      "<c-s>",
-      mode = { "c" },
+      '<c-s>',
+      mode = { 'c' },
       function()
-        require("flash").toggle()
+        require('flash').toggle()
       end,
-      desc = "Toggle Flash Search",
+      desc = 'Toggle Flash Search',
     },
   },
 }
