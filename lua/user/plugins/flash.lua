47f7a124ef6fcd836651cd4eb510f2a2d0a62033
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-07-27 16:31:04 +0200

    fix(flash): added 2char labels to search

--- lua/user/plugins/flash.lua
+++ lua/user/plugins/flash.lua
@@ -7,6 +7,57 @@ local function format(opts)
   }
 end
 
+local twoCharLabel = { after = false, before = { 0, 0 }, uppercase = false, format = format }
+
+local twoCharAction = function(match, state)
+  state:hide()
+  require('flash').jump {
+    search = { max_length = 0 },
+    highlight = { matches = false },
+    label = { format = format },
+    matcher = function(win)
+      -- limit matches to the current label
+      return vim.tbl_filter(function(m)
+        return m.label == match.label and m.win == win
+      end, state.results)
+    end,
+    labeler = function(matches)
+      for _, m in ipairs(matches) do
+        m.label = m.label2 -- use the second label
+      end
+    end,
+  }
+end
+
+local twoCharActionRemote = function(match, state)
+  state:hide()
+  require('flash').remote {
+    search = { max_length = 0 },
+    highlight = { matches = false },
+    label = { format = format },
+    matcher = function(win)
+      -- limit matches to the current label
+      return vim.tbl_filter(function(m)
+        return m.label == match.label and m.win == win
+      end, state.results)
+    end,
+    labeler = function(matches)
+      for _, m in ipairs(matches) do
+        m.label = m.label2 -- use the second label
+      end
+    end,
+  }
+end
+
+local twoCharLabeler = function(matches, state)
+  local labels = state:labels()
+  for m, match in ipairs(matches) do
+    match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+    match.label2 = labels[(m - 1) % #labels + 1]
+    match.label = match.label1
+  end
+end
+
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
@@ -28,35 +79,10 @@ return {
       function()
         require('flash').jump {
           search = { mode = 'search' },
-          label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+          label = twoCharLabel,
           pattern = [[\<]],
-          action = function(match, state)
-            state:hide()
-            require('flash').jump {
-              search = { max_length = 0 },
-              highlight = { matches = false },
-              label = { format = format },
-              matcher = function(win)
-                -- limit matches to the current label
-                return vim.tbl_filter(function(m)
-                  return m.label == match.label and m.win == win
-                end, state.results)
-              end,
-              labeler = function(matches)
-                for _, m in ipairs(matches) do
-                  m.label = m.label2 -- use the second label
-                end
-              end,
-            }
-          end,
-          labeler = function(matches, state)
-            local labels = state:labels()
-            for m, match in ipairs(matches) do
-              match.label1 = labels[math.floor((m - 1) / #labels) + 1]
-              match.label2 = labels[(m - 1) % #labels + 1]
-              match.label = match.label1
-            end
-          end,
+          action = twoCharAction,
+          labeler = twoCharLabeler,
         }
       end,
       desc = 'Flash',
@@ -73,7 +99,13 @@ return {
       'r',
       mode = 'o',
       function()
-        require('flash').remote()
+        require('flash').remote {
+          search = { mode = 'search' },
+          label = twoCharLabel,
+          pattern = [[\<]],
+          action = twoCharActionRemote,
+          labeler = twoCharLabeler,
+        }
       end,
       desc = 'Remote Flash',
     },
