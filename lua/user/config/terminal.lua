f6342cc4e8be09f1bbe61d33278d02f3716b1769
jonas <jonasleonhardfl@gmail.com>
2023-08-07 23:16:40 +0200

    fix(terminal): fixed closing of terminal window not working for errored lazydocker

--- lua/user/config/terminal.lua
+++ lua/user/config/terminal.lua
@@ -12,6 +12,6 @@ end
 
 vim.cmd 'autocmd! TermOpen term://* lua _Set_terminal_keymaps()'
 
--- Automatically close terminal window when one of the following commands exits without error:
+-- Automatically close terminal window when one of the following commands exits:
 -- close after LazyDocker
-vim.cmd([[autocmd TermClose *:lazyDocker if !v:event.status | exe 'bdelete! '..expand('<abuf>') | endif]])
+vim.cmd([[autocmd TermClose *:lazyDocker exe 'bdelete! '..expand('<abuf>')]])
