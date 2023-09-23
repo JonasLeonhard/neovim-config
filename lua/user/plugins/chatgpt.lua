e5f5eb2c036d3e8d6eef8f62686074ce1de36ae5
jonas <jonasleonhardfl@gmail.com>
2023-09-23 12:04:31 +0200

    fix(plugins): increase startuptime performance for chatgpt lua by refactoring to use opts + function require as :lua string

--- lua/user/plugins/chatgpt.lua
+++ lua/user/plugins/chatgpt.lua
@@ -6,9 +6,7 @@ return {
     { '<leader>aA', '<cmd>ChatGPTActAs<cr>', desc = 'ActAs Selection' },
     {
       '<leader>ae',
-      function()
-        require('chatgpt').edit_with_instructions()
-      end,
+      "<cmd>lua require('chatgpt').edit_with_instructions()<cr>",
       desc = 'Edit Line(s) w. Instruction'
     },
     { '<leader>arg', '<cmd>ChatGPTRun grammar_correction<cr>', desc = 'grammar_correction' },
@@ -27,76 +25,72 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope.nvim',
   },
-  config = function()
-    require('chatgpt').setup {
-      edit_with_instructions = {
-        diff = true,
-      },
-      chat = {
-        question_sign = ' ',
-        keymaps = {
-          close = '<C-c>',
-          submit = '<C-s>',
-          yank_last = '<C-y>',
-          yank_last_code = '<C-k>',
-          scroll_up = '<C-u>',
-          scroll_down = '<C-d>',
-          toggle_settings = '<C-o>',
-          new_session = '<C-n>',
-          cycle_windows = '<Tab>',
-        },
-      },
-      popup_layout = {
-        default = 'center',
-        center = {
-          width = '100%',
-          height = '100%',
-        },
-        right = {
-          width = '30%',
-          width_settings_open = '50%',
-        },
-      },
-      popup_window = {
-        filetype = 'chatgpt',
-        border = {
-          highlight = 'GPTBorder',
-          style = 'single',
-          text = {
-            top_align = 'left',
-            top = ' Model ',
-          },
-        },
-        win_options = {
-          winhighlight = 'Normal:TelescopeNormal',
-        },
-      },
-      popup_input = {
-        prompt = ' : ',
-        border = {
-          highlight = 'GPTBorder',
-          style = 'single',
-          text = {
-            top_align = 'left',
-            top = ' Prompt: ['
-                .. 'Submit: <C-s>,'
-                .. ' yank_last/accept: <C-y>,'
-                .. ' yank_last_code: <C-k>,'
-                .. ' cycle_windows: <Tab> '
-                .. ' opts: <C-o> '
-                .. ']',
-          },
-        },
-        win_options = {
-          winhighlight = 'Normal:TelescopeNormal',
-        },
+  opts = {
+    edit_with_instructions = {
+      diff = true,
+    },
+    chat = {
+      question_sign = ' ',
+      keymaps = {
+        close = '<C-c>',
         submit = '<C-s>',
+        yank_last = '<C-y>',
+        yank_last_code = '<C-k>',
+        scroll_up = '<C-u>',
+        scroll_down = '<C-d>',
+        toggle_settings = '<C-o>',
+        new_session = '<C-n>',
+        cycle_windows = '<Tab>',
       },
-      openai_params = {
-        model = "gpt-4",
+    },
+    popup_layout = {
+      default = 'center',
+      center = {
+        width = '100%',
+        height = '100%',
       },
-    }
-
-    vim.api.nvim_set_hl(0, 'GPTBorder', { fg = '#181825' })
-  end,
+      right = {
+        width = '30%',
+        width_settings_open = '50%',
+      },
+    },
+    popup_window = {
+      filetype = 'chatgpt',
+      border = {
+        highlight = 'GPTBorder',
+        style = 'single',
+        text = {
+          top_align = 'left',
+          top = ' Model ',
+        },
+      },
+      win_options = {
+        winhighlight = 'Normal:TelescopeNormal',
+      },
+    },
+    popup_input = {
+      prompt = ' : ',
+      border = {
+        highlight = 'GPTBorder',
+        style = 'single',
+        text = {
+          top_align = 'left',
+          top = ' Prompt: ['
+              .. 'Submit: <C-s>,'
+              .. ' yank_last/accept: <C-y>,'
+              .. ' yank_last_code: <C-k>,'
+              .. ' cycle_windows: <Tab> '
+              .. ' opts: <C-o> '
+              .. ']',
+        },
+      },
+      win_options = {
+        winhighlight = 'Normal:TelescopeNormal',
+      },
+      submit = '<C-s>',
+    },
+    openai_params = {
+      model = "gpt-4",
+    },
+  },
 }
