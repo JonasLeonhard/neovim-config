61d3fcde31d8bb6129cad30a9d2ae4a64d3289db
jonas <jonasleonhardfl@gmail.com>
2023-09-23 22:38:21 +0200

    fix(lsp-progress): removed unneeded callback

--- lua/user/plugins/lsp-progress.lua
+++ lua/user/plugins/lsp-progress.lua
@@ -1,14 +1,5 @@
 return {
   'linrongbin16/lsp-progress.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lsp-progress').setup()
-
-    -- listen lsp-progress event and refresh lualine
-    vim.api.nvim_create_augroup('lualine_augroup', { clear = true })
-    vim.api.nvim_create_autocmd('User LspProgressStatusUpdated', {
-      group = 'lualine_augroup',
-      callback = require('lualine').refresh,
-    })
-  end,
+  opts = {}
 }
