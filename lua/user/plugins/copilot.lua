247ecc9208705fc58c4d8d4ee718a414456aa4f6
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-09-25 16:15:45 +0200

    fix(copilot): fixed panel suggestion deprecated

--- lua/user/plugins/copilot.lua
+++ lua/user/plugins/copilot.lua
@@ -5,43 +5,43 @@ return {
     event = 'InsertEnter',
     lazy = true,
     keys = {
-      { '<leader>ac', '<cmd>CopilotPanel<cr>', desc = 'CopilotPanel' },
+      { '<leader>ac', '<cmd>Copilot panel<cr>', desc = 'CopilotPanel' },
       {
         '<leader>a<Enter>',
         function()
           require('copilot.panel').accept()
         end,
-        desc = 'Accept CopilotPanel suggestion'
+        desc = 'Accept CopilotPanel suggestion',
       },
       {
         '<leader>an',
         function()
           require('copilot.panel').jump_next()
         end,
-        desc = 'Accept CopilotPanel suggestion'
+        desc = 'Accept CopilotPanel suggestion',
       },
       {
         '<leader>ap',
         function()
           require('copilot.panel').jump_prev()
         end,
-        desc = 'Accept CopilotPanel suggestion'
+        desc = 'Accept CopilotPanel suggestion',
       },
       {
         '<leader>aR',
         function()
           require('copilot.panel').refresh()
         end,
-        desc = 'Accept CopilotPanel suggestion'
+        desc = 'Accept CopilotPanel suggestion',
       },
     },
     opts = {
-      suggestion = { enabled = false },   -- suggestion disabled because it's conflicting with cmp ghost text
+      suggestion = { enabled = false }, -- suggestion disabled because it's conflicting with cmp ghost text
       panel = {
         layout = {
           ratio = 0.6,
         },
       },
-    }
+    },
   },
 }
