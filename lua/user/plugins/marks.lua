087c5b37a5824e3ff0107ada3a7e3e893874ecf7
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2024-01-29 15:47:23 +0100

    fix(marks): fixed refresh interval

--- lua/user/plugins/marks.lua
+++ lua/user/plugins/marks.lua
@@ -30,7 +30,7 @@ return {
   },
   opts = {
     -- while lower values may cause performance penalties. default 150.
-    refresh_interval = 10000,
+    refresh_interval = 1000,
     -- whether the shada file is updated after modifying marks. default false. Without this cleared marks are reloaded after saving.
     force_write_shada = true,
     sign_priority = 100,
