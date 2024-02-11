d62d21e3c5855c82c89cb2b7f0aa8ccee0c08cc8
jonas <jonasleonhardfl@gmail.com>
2024-02-11 20:18:54 +0100

    fix(broil): add broil config #testing

--- lua/user/plugins/broil.lua
+++ lua/user/plugins/broil.lua
@@ -1,8 +1,21 @@
 return {
   'JonasLeonhard/broil',
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
   opts = {
-    test = true
+    mappings = {
+      synchronize = '<C-y>',
+      help = '?',
+    }
   },
   dev = true, -- This will use {config.dev.path}/broil/ instead of fetching it from Github
+  keys = {
+    {
+      '<leader>o',
+      "<cmd>lua require('broil').open()<cr>",
+      desc = 'Broil open',
+    },
+  },
   enabled = false
 }
