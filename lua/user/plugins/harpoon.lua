eeb3214cd385b608eb03ba416f3db968a1dae5e5
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2024-02-20 10:12:04 +0100

    fix(marks): fixed mark keybinds

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -3,57 +3,52 @@ return {
   lazy = true,
   keys = {
     {
-      '<leader>m,',
-      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
-      desc = 'Harpoon (key: ",")',
-    },
-    {
-      ',,',
+      '<leader>mm',
       "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
       desc = 'Marks (builtin)',
     },
     {
-      ',a',
+      '<leader>ma',
       "<cmd>lua require('harpoon.mark').add_file()<cr>",
       desc = 'Add File Mark',
     },
     {
-      ',d',
+      '<leader>md',
       "<cmd>lua require('harpoon.mark').toggle_file()<cr>",
       desc = 'Toggle File Mark',
     },
     {
-      ',n',
+      '<leader>mn',
       "<cmd>lua require('harpoon.ui').nav_next()<cr>",
       desc = 'Next Mark',
     },
     {
-      ',b',
+      '<leader>mb',
       "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
       desc = 'Prev Mark',
     },
     {
-      ',1',
+      '<leader>m1',
       "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
       desc = 'Navigate to Harpoon Mark (1)',
     },
     {
-      ',2',
+      '<leader>m2',
       "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
       desc = 'Navigate to Harpoon Mark (2)',
     },
     {
-      ',3',
+      '<leader>m3',
       "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
       desc = 'Navigate to Harpoon Mark (3)',
     },
     {
-      ',4',
+      '<leader>m4',
       "lua require('harpoon.ui').nav_file(4)<cr>",
       desc = 'Navigate to Harpoon Mark (4)',
     },
     {
-      ',5',
+      '<leader>m5',
       "<cmd>lua require('harpoon.ui').nav_file(5)<cr>",
       desc = 'Navigate to Harpoon Mark (5)',
     },
