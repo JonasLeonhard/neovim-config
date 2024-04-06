5f7d6034eb2b5701fc71d867856c2843bac3ed67
jonas <jonasleonhardfl@gmail.com>
2024-04-06 11:21:58 +0200

    fix(comment): we can remove comment.nvim because nvim 0.10 nightly has the comemnd functionality builtin

--- lua/user/plugins/comment.lua
+++ lua/user/plugins/comment.lua
@@ -1,14 +1,3 @@
 return {
-  -- "gc" to comment visual regions/lines
-  {
-    'numToStr/Comment.nvim',
-    event = 'User FileOpened',
-    lazy = true,
-    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
-    config = function()
-      require('Comment').setup {
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-      }
-    end,
-  },
+  'JoosepAlviste/nvim-ts-context-commentstring',
 }
