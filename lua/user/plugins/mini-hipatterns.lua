f90fcf73235e0a9c497b340d22690a4b5ca808d6
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2024-07-23 08:57:43 +0200

    fix: added INFO to hipatterns

--- lua/user/plugins/mini-hipatterns.lua
+++ lua/user/plugins/mini-hipatterns.lua
@@ -2,19 +2,20 @@ return {
   'echasnovski/mini.hipatterns',
   version = '*',
   lazy = true,
-  event = "VeryLazy",
+  event = 'VeryLazy',
   opts = function()
-    local hipatterns = require('mini.hipatterns')
+    local hipatterns = require 'mini.hipatterns'
     return {
       highlighters = {
-        fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-        hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-        todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-        note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+        fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+        hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+        todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+        note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+        info = { pattern = '%f[%w]()INFO()%f[%W]', group = 'MiniHipatternsNote' },
 
         -- Highlight hex color strings (`#rrggbb`) using that color
         hex_color = hipatterns.gen_highlighter.hex_color(),
-      }
+      },
     }
   end,
 }
