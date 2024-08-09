7dd6ddd198bcd6dc6dc8556711782eea46c68e69
jonas <jonasleonhardfl@gmail.com>
2024-08-09 13:17:40 +0200

    fix(quicker): change bqf to quicker

---
+++ lua/user/plugins/quicker.lua
@@ -0,0 +1,38 @@
+return {
+  'stevearc/quicker.nvim',
+  lazy = true,
+  ft = "qf",
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    opts = {
+      relativenumber = true,
+    },
+    keys = {
+      {
+        "<TAB>",
+        function()
+          require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+        end,
+        desc = "Expand quickfix context",
+      },
+      {
+        "<S-TAB>",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse quickfix context",
+      },
+    }
+  },
+  keys = {
+    {
+      '<leader>uq',
+      function()
+        require('quicker').toggle({ focus = true })
+      end,
+      desc = 'Toggle Quicker',
+      mode = 'n',
+    },
+  },
+}
