e477b58100975e1c96828ac9137408059b8c63ac
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2024-08-22 09:53:10 +0200

    fix(server_settings): enable tsserver for jsx

--- lua/user/plugins/lsp/helpers/server_settings.lua
+++ lua/user/plugins/lsp/helpers/server_settings.lua
@@ -38,16 +38,17 @@ return {
     init_options = {
       plugins = {
         {
-          name = "@vue/typescript-plugin",
-          location = os.getenv('HOME') .. "/.config/yarn/global/node_modules/@vue/typescript-plugin",
-          languages = { "javascript", "typescript", "vue" },
+          name = '@vue/typescript-plugin',
+          location = os.getenv 'HOME' .. '/.config/yarn/global/node_modules/@vue/typescript-plugin',
+          languages = { 'javascript', 'typescript', 'vue' },
         },
       },
     },
     filetypes = {
-      "javascript",
-      "typescript",
-      "vue",
+      'javascript',
+      'typescript',
+      'vue',
+      'javascriptreact',
     },
     settings = {
       javascript = {
@@ -77,28 +78,67 @@ return {
   tailwindcss = {
     filetypes = {
       'templ',
-      "aspnetcorerazor", "astro", "astro-markdown", "blade", "django-html", "edge", "eelixir", "ejs", "erb", "eruby",
-      "gohtml", "haml", "handlebars", "hbs", "html", "html-eex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache",
-      "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss",
-      "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte"
+      'aspnetcorerazor',
+      'astro',
+      'astro-markdown',
+      'blade',
+      'django-html',
+      'edge',
+      'eelixir',
+      'ejs',
+      'erb',
+      'eruby',
+      'gohtml',
+      'haml',
+      'handlebars',
+      'hbs',
+      'html',
+      'html-eex',
+      'jade',
+      'leaf',
+      'liquid',
+      'markdown',
+      'mdx',
+      'mustache',
+      'njk',
+      'nunjucks',
+      'php',
+      'razor',
+      'slim',
+      'twig',
+      'css',
+      'less',
+      'postcss',
+      'sass',
+      'scss',
+      'stylus',
+      'sugarss',
+      'javascript',
+      'javascriptreact',
+      'reason',
+      'rescript',
+      'typescript',
+      'typescriptreact',
+      'vue',
+      'svelte',
       -- include any other filetypes where you need tailwindcss
     },
     init_options = {
       userLanguages = {
-        templ = "html"
-      }
-    }
+        templ = 'html',
+      },
+    },
   },
   html = {
     filetypes = {
-      'templ'
-    }
+      'templ',
+    },
   },
   zls = {
     -- https://kristoff.it/blog/improving-your-zls-experience/
     settings = {
       enable_build_on_save = true,
-      build_on_save_step = "check"
-    }
-  }
+      build_on_save_step = 'check',
+    },
+  },
 }
