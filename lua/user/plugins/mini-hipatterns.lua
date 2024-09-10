f532e491bde043a2767225f3dc4310653cbb4cbf
jonas <jonasleonhardfl@gmail.com>
2024-09-10 17:00:27 +0200

    fix(hipatterns): added SPEED

--- lua/user/plugins/mini-hipatterns.lua
+++ lua/user/plugins/mini-hipatterns.lua
@@ -12,6 +12,7 @@ return {
         todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
         note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
         info = { pattern = '%f[%w]()INFO()%f[%W]', group = 'MiniHipatternsNote' },
+        speed = { pattern = '%f[%w]()SPEED()%f[%W]', group = 'MiniHipatternsHack' },
 
         -- Highlight hex color strings (`#rrggbb`) using that color
         hex_color = hipatterns.gen_highlighter.hex_color(),
