87881b04b6cfc284ad924c41495f3d7cbbc91cc7
jonas <jonasleonhardfl@gmail.com>
2024-09-15 19:50:09 +0200

    fix(init): set binding to netrw only

--- lua/user/config/init.lua
+++ lua/user/config/init.lua
@@ -5,50 +5,50 @@ vim.g.maplocalleader = ' '
 
 local opt = vim.opt
 
-opt.smartindent = true -- smartly indent new lines
-opt.breakindent = true -- keep same indentation after break
-opt.autowrite = true -- Enable auto write
-opt.clipboard = 'unnamedplus' -- Sync with system clipboard
+opt.smartindent = true         -- smartly indent new lines
+opt.breakindent = true         -- keep same indentation after break
+opt.autowrite = true           -- Enable auto write
+opt.clipboard = 'unnamedplus'  -- Sync with system clipboard
 opt.completeopt = 'menu,menuone,noselect'
-opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
-opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true -- Use spaces instead of tabs
+opt.conceallevel = 2           -- Hide * markup for bold and italic, but not markers with substitutions
+opt.confirm = true             -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true          -- Enable highlighting of the current line
+opt.expandtab = true           -- Use spaces instead of tabs
 opt.formatoptions = 'jcroqlnt' -- tcqj
 opt.grepformat = '%f:%l:%c:%m'
 opt.grepprg = 'rg --vimgrep'
-opt.ignorecase = true -- Ignore case
+opt.ignorecase = true      -- Ignore case
 opt.inccommand = 'nosplit' -- preview incremental substitute
-opt.list = true -- Show some invisible characters (tabs...
-opt.mouse = 'a' -- Enable mouse mode
-opt.number = true -- Print line number
-opt.pumblend = 10 -- Popup blend
-opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.relativenumber = true -- Relative line numbers
-opt.scrolloff = 4 -- Lines of context
+opt.list = true            -- Show some invisible characters (tabs...
+opt.mouse = 'a'            -- Enable mouse mode
+opt.number = true          -- Print line number
+opt.pumblend = 10          -- Popup blend
+opt.pumheight = 10         -- Maximum number of entries in a popup
+opt.relativenumber = true  -- Relative line numbers
+opt.scrolloff = 4          -- Lines of context
 opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp', 'folds' }
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
+opt.shiftround = true      -- Round indent
+opt.shiftwidth = 2         -- Size of an indent
 opt.shortmess:append { W = true, I = true, c = true, C = true }
-opt.showmode = false -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8 -- Columns of context
-opt.signcolumn = 'yes' -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
+opt.showmode = false       -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8      -- Columns of context
+opt.signcolumn = 'yes'     -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true       -- Don't ignore case with capitals
+opt.smartindent = true     -- Insert indents automatically
 opt.spelllang = { 'en' }
-opt.splitbelow = true -- Put new windows below current
+opt.splitbelow = true      -- Put new windows below current
 opt.splitkeep = 'screen'
-opt.splitright = true -- Put new windows right of current
-opt.tabstop = 2 -- Number of spaces tabs count for
-opt.termguicolors = true -- True color support
+opt.splitright = true      -- Put new windows right of current
+opt.tabstop = 2            -- Number of spaces tabs count for
+opt.termguicolors = true   -- True color support
 opt.timeoutlen = 300
 opt.undofile = true
 opt.undolevels = 10000
-opt.updatetime = 200 -- Save swap file and trigger CursorHold
-opt.virtualedit = 'block' -- Allow cursor to move where there is no text in visual block mode
+opt.updatetime = 200               -- Save swap file and trigger CursorHold
+opt.virtualedit = 'block'          -- Allow cursor to move where there is no text in visual block mode
 opt.wildmode = 'longest:full,full' -- Command-line completion mode
-opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
+opt.winminwidth = 5                -- Minimum window width
+opt.wrap = false                   -- Disable line wrap
 
 opt.laststatus = 3
 opt.ch = 0 -- no cmdheight
@@ -99,5 +99,12 @@ _G.Netrw_create_file = function()
     print 'File name cannot be empty'
   end
 end
-vim.cmd 'autocmd FileType netrw nnoremap ? :help netrw-quickmap<CR>'
-vim.cmd 'autocmd FileType netrw nnoremap <silent> f :lua Netrw_create_file()<CR>'
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    -- Set up buffer-local keybindings
+    vim.api.nvim_buf_set_keymap(0, 'n', '?', ':help netrw-quickmap<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', 'f', ':lua _G.Netrw_create_file()<CR>', { noremap = true, silent = true })
+  end
+})
