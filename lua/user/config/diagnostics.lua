dc5d0544baae4769fb1f5910036e64f286cf5005
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2024-12-05 12:28:21 +0100

    fix(diagnostics): use non deprecated api to define diagnostic signs

--- lua/user/config/diagnostics.lua
+++ lua/user/config/diagnostics.lua
@@ -1,15 +1,16 @@
-local signs = { Error = ' ', Warn = ' ', Hint = '󰋼 ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 vim.diagnostic.config {
   float = {
     source = 'if_many',
   },
   severity_sort = true,
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = ' ',
+      [vim.diagnostic.severity.WARN] = ' ',
+      [vim.diagnostic.severity.HINT] = '󰋼 ',
+      [vim.diagnostic.severity.INFO] = ' ',
+    },
+  },
   underline = true,
   update_in_insert = false,
   virtual_text = {
