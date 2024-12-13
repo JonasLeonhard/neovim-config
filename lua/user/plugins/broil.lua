ee6e89b7b0b941b03f019b95afe4b891a8331bff
jonas <jonasleonhardfl@gmail.com>
2024-12-13 10:18:00 +0100

    fix: use broil

---
+++ lua/user/plugins/broil.lua
@@ -0,0 +1,31 @@
+return {
+  'JonasLeonhard/broil',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+      build =
+      'make'
+    }
+  },
+  opts = {
+    -- rm_command = 'rm <FROM>', -- optional...(default 'rm'). you could use a trash command here. Or rm --trash for nushell...
+    -- ... you can find more opts in ":h broil" or lua/broil/config.lua
+  },
+  keys = {
+    {
+      '<leader>o',
+      "<cmd>lua require('broil').open()<cr>", -- opens current %:h or cwd by default
+      desc = 'Broil open',
+    },
+    {
+      '<leader>O',
+      "<cmd>lua require('broil').open(vim.fn.getcwd())<cr>",
+      desc = 'Broil open cwd',
+    },
+  }
+}
