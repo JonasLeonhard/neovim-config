f1a558adae862424436f107ad0a893eb4adeab29
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2025-02-13 17:21:10 +0100

    fix: set neogit to VeryLazy startup

--- lua/user/plugins/neogit.lua
+++ lua/user/plugins/neogit.lua
@@ -1,13 +1,13 @@
 return {
   'NeogitOrg/neogit',
   cmd = 'Neogit',
+  event = "VeryLazy",
   dependencies = 'nvim-lua/plenary.nvim',
   opts = {
     commit_editor = {
       show_staged_diff = false, -- INFO: Disabled because this freezes neogit for very large commits
     }
   },
-  lazy = true,
   keys = {
     {
       '<leader>gg',
