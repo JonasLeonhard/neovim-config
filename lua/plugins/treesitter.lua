2fb4cf9eab5570e8d825f08252d25087c4c9e682
jonas <jonasleonhardfl@gmail.com>
2025-04-08 21:45:26 +0200

    feat: remove textobjects, move autotag setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-local disable_func = function(lang, buf)
+local disable_func = function(_lang, buf)
   local max_line_width = 500
 
   -- Check first line width
@@ -13,20 +13,17 @@ return {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
       {
-        "chrisgrieser/nvim-various-textobjs",
-        opts = { keymaps = { useDefaults = true } },
-      },
+        'windwp/nvim-ts-autotag',
+        event = 'InsertEnter',
+        lazy = true,
+        opts = {},
+      }
     },
     event = 'VeryLazy',
     lazy = true,
     opts = {
       additional_vim_regex_highlighting = false,
-      ensure_installed = { 'go', 'lua', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'regex', 'bash', 'markdown_inline',
-        'markdown' },
-      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-      auto_install = true,
       highlight = {
         enable = true,
         disable = disable_func,
@@ -41,49 +38,10 @@ return {
           node_decremental = '<S-TAB>',
         },
       },
-      textobjects = {
-        select = {
-          enable = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ao"] = "@block.outer",
-            ["io"] = "@block.inner",
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
-            ["aC"] = "@conditional.outer",
-            ["iC"] = "@conditional.inner",
-            ["aO"] = "@class.outer",
-            ["iO"] = "@class.inner",
-            ["aA"] = "@assignment.inner",
-            ["aX"] = "@assignment.lhs",
-            ["aY"] = "@assignment.rhs",
-            ["aG"] = "@comment.outer",
-            ["iG"] = "@comment.inner",
-            ["an"] = "@number.outer",
-            ["in"] = "@number.inner",
-            ["ar"] = "@return.outer",
-            ["ir"] = "@return.inner",
-            ["aR"] = "@regex.outer",
-            ["iR"] = "@regex.inner",
-            ["as"] = "@statement.outer",
-            ["is"] = "@statement.inner",
-            ["iS"] = "@scopename.inner",
-            ["aS"] = "@scopename.outer",
-            ["aP"] = "@parameter.outer",
-            ["iP"] = "@parameter.inner",
-            ["ia"] = "@attribute.inner",
-            ["aa"] = "@attribute.outer",
-            ["ac"] = "@call.outer",
-            ["ic"] = "@call.inner"
-          }
-        }
-      }
     },
     build = ':TSUpdate',
     config = function(_, opts)
       require('nvim-treesitter.configs').setup(opts)
-      vim.cmd('doautocmd User TreesitterLoaded')
     end,
   },
 }
