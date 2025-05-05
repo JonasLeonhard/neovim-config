f6894eab707c97e2185b4e2ef12fec438b73f49d
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2025-05-05 12:04:55 +0200

    fix: fold text syntax highlighting

--- lua/options.lua
+++ lua/options.lua
@@ -65,6 +65,7 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.foldtext = '' -- keep the treesitter syntax highlighting for folds
 
 -- Filetype's:
 vim.filetype.add({
