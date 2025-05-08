78e38ae917d7f6b596e952de13fac6c22d14aa82
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2025-05-08 09:35:30 +0200

    fix: update to mason 2.0

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,6 +1,5 @@
 local mason_registry = require('mason-registry')
-local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() ..
-    '/node_modules/@vue/language-server'
+local vue_language_server_path = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server")
 
 return {
   cmd = { 'typescript-language-server', '--stdio' },
