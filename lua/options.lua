3d5993ac2c3780f9d3503c5558c5a73e85c07f5c
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2025-06-24 08:50:35 +0200

    fix: vim deprecated vim.highlight -> vim.hl

--- lua/options.lua
+++ lua/options.lua
@@ -119,7 +119,7 @@ end, {})
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.highlight.on_yank { higroup = 'YankHighlight', priority = 10000 }
+    vim.hl.on_yank { higroup = 'YankHighlight', priority = 10000 }
   end,
   group = highlight_group,
   pattern = '*',
