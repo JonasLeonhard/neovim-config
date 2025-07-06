4041157fed54c5f16d2dcb43cc7ea86aee8bc949
jonas <jonasleonhardfl@gmail.com>
2025-07-06 15:00:01 +0200

    fix: ols better checker default args

--- lsp/ols.lua
+++ lsp/ols.lua
@@ -3,6 +3,6 @@ return {
   filetypes = { 'odin' },
   root_dir = vim.fs.root(0, { 'ols.json', '.git', '.odin' }),
   init_options = {
-    checker_args = "-strict-style",
+    checker_args = "-strict-style -vet -debug",
   },
 }
